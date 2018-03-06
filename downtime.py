34064b8f2f62131a8cc25a75e460a19f687d0a37
maduck <utti+github@uttisoft.de>
2018-03-06 18:41:37 +0100

    adjust helper script. still not happy.

--- downtime.py
+++ downtime.py
@@ -2,46 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import time
-import sys
 # ====================== Configuration ==========================================
-commandfile = '/var/lib/nagios3/rw/nagios.cmd'
+command_file = '/var/lib/nagios3/rw/nagios.cmd'
 # ====================== Configuration ==========================================
 
-def read_input(message, default_value=None, result_type=str):
-    """ helper function for reading input """
-    result = None
-    if default_value:
-        try:
-            result = input("%s [%s]: " % (message, default_value))
-        except KeyboardInterrupt:
-            sys.exit(0)
-        except (KeyboardInterrupt, EOFError):
-            pass
-        if not result:
-            result = default_value
-    else:
-        while not result:
-            try:
-                result = input("%s: " % message)
-            except KeyboardInterrupt:
-                sys.exit(0)
-            except (KeyboardInterrupt, EOFError):
-                pass
-    try:
-        result = result_type(result)
-    except:
-        return None
-    return result
-
-hosts = read_input("Hostname, durch Komma getrennt").split(',')
-downtime_length = read_input("Länge der Downtime in Stunden", 2, float) * 60 * 60
+hosts = input("Hostnames, comma delimited").split(',')
+downtime_length = input("Länge der Downtime in Stunden")
+if not downtime_length:
+    downtime_length = 2
+downtime_length = float(downtime_length) * 60 * 60
 downtime_start = time.time()
 downtime_end = downtime_start + downtime_length
 
-
-#/usr/bin/printf "[%lu] SCHEDULE_HOST_DOWNTIME;%s;%lu;%lu;0;0;7200;nagios console;Reboot\n" $now $host $now $then > $commandfile
-#/usr/bin/printf "[%lu] SCHEDULE_HOST_SVC_DOWNTIME;%s;%lu;%lu;0;0;7200;nagios console;Reboot\n" $now $host $now $then > $commandfile
-
 for host in hosts:
-    print("[%s] SCHEDULE_HOST_DOWNTIME;%s;%s;%s;0;0;%0.0f;nagios console;Reboot" % (downtime_start, host, downtime_start, downtime_end, downtime_length))
-    print("[%s] SCHEDULE_HOST_SVC_DOWNTIME;%s;%s;%s;0;0;%0.0f;nagios console;Reboot" % (downtime_start, host, downtime_start, downtime_end, downtime_length))
+    params = (downtime_start, host, downtime_start, downtime_end, downtime_length)
+    print("[%s] SCHEDULE_HOST_DOWNTIME;%s;%s;%s;0;0;%0.0f;nagios console;Reboot" % params)
+    print("[%s] SCHEDULE_HOST_SVC_DOWNTIME;%s;%s;%s;0;0;%0.0f;nagios console;Reboot" % params)
