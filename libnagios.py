027d6f38ff63236b34dd98eaaf1a6537bd6c830e
maduck <utti+github@uttisoft.de>
2018-03-09 19:44:16 +0100

    change the string formatting from python2 to python3 style

--- libnagios.py
+++ libnagios.py
@@ -45,7 +45,7 @@ class CheckVariable(object):
             self.value = self.var_type(self.check_result)
             self.value = self.pre_processor(self.value)
         except Exception as e:
-            logging.debug("Preprocessor failed: %s" % e)
+            logging.debug("Preprocessor failed: {}".format(e))
 
     def set_state(self):
         if self.value is None:
@@ -55,18 +55,18 @@ class CheckVariable(object):
         for state, evaluation in enumerate(evaluations):
             if evaluation and evaluation(self.value):
                 self.nagios_state = States(state)
-        logging.debug("Variable %s yields nagios state %r" % (self.name, state))
+        logging.debug("Variable {} yields nagios state {}".format(self.name, self.nagios_state.name))
 
     def pretty_format(self):
         result = str(self)
         if self.unit and self.value is not None:
-            result = "%s %s" % (result, self.unit)
+            result = "{} {}".format(result, self.unit)
         return result
 
     def __str__(self):
         result = "%s" % self.value
         if self.var_type == float and self.value is not None:
-            result = "%0.2f" % self.value
+            result = "{:.2f}".format(self.value)
         return result
 
 
@@ -87,7 +87,7 @@ class Nagios(object):
 
     def add_check_variable(self, var_name, var_type, unit='', ok_condition=lambda x: True, warn_condition=None,
                            crit_condition=None, pre_processor=None):
-        logging.debug("adding variable %s (%s)" % (var_name, var_type.__name__))
+        logging.debug("adding variable {} ({})".format(var_name, var_type.__name__))
         if len(self.check_variables) == 0:
             self.main = var_name
         self.check_variables[var_name] = CheckVariable(var_name, var_type, unit, ok_condition,
@@ -96,10 +96,10 @@ class Nagios(object):
     def add_check_result(self, var_name, check_result):
         variable = self.check_variables.get(var_name)
         if variable is not None:
-            logging.debug("Adding check result '%s' to variable %s" % (check_result, var_name))
+            logging.debug("Adding check result '{}' to variable {}".format(check_result, var_name))
             variable.set_check_result(check_result)
         else:
-            logging.debug("Not knowing variable %s, not adding check result." % var_name)
+            logging.debug("Not knowing variable {}, not adding check result.".format(var_name))
 
     def generate_performance_data(self):
         performance_datapoints = []
@@ -108,7 +108,7 @@ class Nagios(object):
             if var.has_perfdata():
                 performance_datapoints.append(var.get_perfdata())
         if performance_datapoints:
-            output = " | %s" % ", ".join(performance_datapoints)
+            output = " | {}".format(", ".join(performance_datapoints))
         return output
 
     def generate_return_code(self):
@@ -123,8 +123,8 @@ class Nagios(object):
     def generate_output(self, override_message=None):
         return_code, var_output = self.generate_return_code()
         state = States(return_code).name
-        output = "%s %s - %s" % (self.service_name, state, var_output)
+        output = "{} {} - {}".format(self.service_name, state, var_output)
         if override_message is not None:
-            output = "%s %s - %s" % (self.service_name, state, override_message.strip())
+            output = "{} {} - {}".format(self.service_name, state, override_message.strip())
         output += self.generate_performance_data()
         return return_code, output
