9a3bf8de0e18b7b60b5fed212f19aaf678dbd669
MaDuck <maduck@users.noreply.github.com>
2018-11-11 09:18:14 +0100

    Refactor downtime tool to make use of argparse

--- downtime.py
+++ downtime.py
@@ -1,20 +1,37 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import time
-# ====================== Configuration ==========================================
-command_file = '/var/lib/nagios3/rw/nagios.cmd'
-# ====================== Configuration ==========================================
+import argparse
 
-hosts = input("Hostnames, comma delimited").split(',')
-downtime_length = input("Länge der Downtime in Stunden")
-if not downtime_length:
-    downtime_length = 2
-downtime_length = float(downtime_length) * 60 * 60
+parser = argparse.ArgumentParser(description="Schedule a downtime for a host and all of its services.")
+parser.add_argument("--hosts", "-H", nargs="+", help="list of hosts to schedule a downtime for", required=True)
+parser.add_argument("--duration", "-D", type=int, default=2, help="Downtime duration, defaults to %(default)d")
+parser.add_argument("--commandfile", "-c", default="/var/lib/nagios3/rw/nagios.cmd", type=argparse.FileType('a'),
+                    help="Location of Nagios command file, defaults to %(default)s")
+
+
+args = parser.parse_args()
+
+downtime_length = float(args.duration) * 60 * 60
 downtime_start = time.time()
 downtime_end = downtime_start + downtime_length
 
-for host in hosts:
-    params = (downtime_start, host, downtime_start, downtime_end, downtime_length)
-    print("[%s] SCHEDULE_HOST_DOWNTIME;%s;%s;%s;0;0;%0.0f;nagios console;Reboot" % params)
-    print("[%s] SCHEDULE_HOST_SVC_DOWNTIME;%s;%s;%s;0;0;%0.0f;nagios console;Reboot" % params)
+host_downtime = "[{}] SCHEDULE_HOST_DOWNTIME".format(downtime_start)
+service_downtime = "[{}] SCHEDULE_HOST_SVC_DOWNTIME".format(downtime_start)
+
+
+for host in args.hosts:
+    nagios_parameters = [
+        host,
+        "{:0.0f}".format(downtime_start),
+        "{:0.0f}".format(downtime_end),
+        "0",
+        "0",
+        "{:0.0f}".format(downtime_length),
+        "libnagios",
+        "CLI command",
+    ]
+
+    args.commandfile.write(";".join([host_downtime] + nagios_parameters) + "\n")
+    args.commandfile.write(";".join([service_downtime] + nagios_parameters) + "\n")
